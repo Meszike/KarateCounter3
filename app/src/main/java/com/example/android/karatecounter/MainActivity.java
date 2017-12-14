cd86eea6b64e11766e3b982ec00322658fdc79aa
Meszike <poborik@gmail.com>
2017-12-14 17:16:06 +0100

    New version

--- app/src/main/java/com/example/android/karatecounter/MainActivity.java
+++ app/src/main/java/com/example/android/karatecounter/MainActivity.java
@@ -22,74 +22,94 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (MagicButton)findViewById(R.id.threebutton);
+        /**
+         * Increase the score for Red Team by 3 points.
+         */
+        button = (MagicButton) findViewById(R.id.threebutton);
         button.setMagicButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                scoreTeamA=scoreTeamA+3;
+                scoreTeamA = scoreTeamA + 3;
                 displayForTeamA(scoreTeamA);
             }
         });
-
-        button = (MagicButton)findViewById(R.id.twobutton);
+        /**
+        * Increase the score for Red Team by 2 points.
+        */
+        button = (MagicButton) findViewById(R.id.twobutton);
         button.setMagicButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                scoreTeamA=scoreTeamA+2;
+                scoreTeamA = scoreTeamA + 2;
                 displayForTeamA(scoreTeamA);
             }
         });
-
-        button = (MagicButton)findViewById(R.id.onebutton);
+        /**
+        * Increase the score for Red Team by 1 point.
+        */
+        button = (MagicButton) findViewById(R.id.onebutton);
         button.setMagicButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                scoreTeamA=scoreTeamA+1;
+                scoreTeamA = scoreTeamA + 1;
                 displayForTeamA(scoreTeamA);
             }
         });
-
-        button = (MagicButton)findViewById(R.id.warninga);
+        /**
+        * Increase the warning for Red Team by 1 in warning.
+        */
+        button = (MagicButton) findViewById(R.id.warninga);
         button.setMagicButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                WarningA=WarningA+1;
+                WarningA = WarningA + 1;
                 displayForRedTeam(WarningA);
             }
         });
 
-        button = (MagicButton)findViewById(R.id.threebuttonb);
+        /**
+         * Increase the score for Blue Team by 3 points.
+         */
+        button = (MagicButton) findViewById(R.id.threebuttonb);
         button.setMagicButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ScoreTeamB=ScoreTeamB+3;
+                ScoreTeamB = ScoreTeamB + 3;
+                displayForTeamB(ScoreTeamB);
+            }
+        });
+        /**
+         * Increase the score for Blue Team by 2 points.
+         */
+        button = (MagicButton) findViewById(R.id.twobuttonb);
+        button.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ScoreTeamB = ScoreTeamB + 2;
+                displayForTeamB(ScoreTeamB);
+            }
+        });
+        /**
+         * Increase the score for Blue Team by 1 point.
+         */
+        button = (MagicButton) findViewById(R.id.onebuttonb);
+        button.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ScoreTeamB = ScoreTeamB + 1;
                 displayForTeamB(ScoreTeamB);
             }
         });
 
-        button = (MagicButton)findViewById(R.id.twobuttonb);
-        button.setMagicButtonClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ScoreTeamB=ScoreTeamB+2;
-                displayForTeamB(ScoreTeamB);
-            }
-        });
+        /**
+        * Increase the warning for Blue Team by 1 point in warning.
+        */
 
-        button = (MagicButton)findViewById(R.id.onebuttonb);
+        button = (MagicButton) findViewById(R.id.warningb);
         button.setMagicButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ScoreTeamB=ScoreTeamB+1;
-                displayForTeamB(ScoreTeamB);
-            }
-        });
-
-        button = (MagicButton)findViewById(R.id.warningb);
-        button.setMagicButtonClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                WarningB=WarningB+1;
+                WarningB = WarningB + 1;
                 displayForBlueTeam(WarningB);
             }
         });
@@ -113,68 +133,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Increase the score for Red Team by 1 point.
-     */
-    public void addOneForTeamA(View v) {
-        scoreTeamA=scoreTeamA+1;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     * Increase the score for Red Team by 2 points.
-     */
-    public void addTwoForTeamA(View v) {
-        scoreTeamA=scoreTeamA+2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     * Increase the score for Red Team by 3 points.
-     */
-    public void addThreeForTeamA(View v) {
-        scoreTeamA=scoreTeamA+3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
      * Displays the given warning for Red Team.
      */
     public void displayForRedTeam(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_warning);
         scoreView.setText(String.valueOf(score));
-    }
-
-    /**
-     * Increase the warning for Red Team by 1 i n warning.
-     */
-    public void addOneWarningForA(View v) {
-        WarningA=WarningA+1;
-        displayForRedTeam(WarningA);
-    }
-
-
-    /**
-     * Increase the score for Blue Team by 1 point.
-     */
-    public void addOneForTeamB(View v) {
-        ScoreTeamB=ScoreTeamB+1;
-        displayForTeamB(ScoreTeamB);
-    }
-
-    /**
-     * Increase the score for Blue Team by 2 points.
-     */
-    public void addTwoForTeamB(View v) {
-        ScoreTeamB=ScoreTeamB+2;
-        displayForTeamB(ScoreTeamB);
-    }
-
-    /**
-     * Increase the score for Blue Team by 3 points.
-     */
-    public void addThreeForTeamB(View v) {
-        ScoreTeamB=ScoreTeamB+3;
-        displayForTeamB(ScoreTeamB);
     }
 
 
@@ -186,19 +149,13 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Increase the warning for Blue Team by 1 point in warning.
-     */
-    public void addOneWarningForBlue(View v) {
-        WarningB=WarningB+1;
-        displayForBlueTeam(WarningB);
-    }
+
     /**
      * Delete the score for both Team points and warnings.
      */
     public void Reset(View v) {
-        scoreTeamA=0;
-        ScoreTeamB=0;
+        scoreTeamA = 0;
+        ScoreTeamB = 0;
         WarningA = 0;
         WarningB = 0;
         displayForTeamB(0);
@@ -207,9 +164,11 @@ public class MainActivity extends AppCompatActivity {
         displayForBlueTeam(0);
     }
 
-    /** save counter and on device rotation*/
+    /**
+     * save counter and on device rotation
+     */
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState){
+    public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt("saveA", scoreTeamA);
         savedInstanceState.putInt("saveB", ScoreTeamB);
@@ -217,13 +176,15 @@ public class MainActivity extends AppCompatActivity {
         savedInstanceState.putInt("savedB", WarningB);
     }
 
-    /** restore score and after rotation*/
+    /**
+     * restore score and after rotation
+     */
 
     @Override
-    public void onRestoreInstanceState (Bundle saveInstanceState){
+    public void onRestoreInstanceState(Bundle saveInstanceState) {
         super.onRestoreInstanceState(saveInstanceState);
 
-        if (saveInstanceState != null){
+        if (saveInstanceState != null) {
             scoreTeamA = saveInstanceState.getInt("saveA");
             ScoreTeamB = saveInstanceState.getInt("saveB");
             WarningA = saveInstanceState.getInt("savedA");
@@ -235,27 +196,6 @@ public class MainActivity extends AppCompatActivity {
         displayForRedTeam(WarningA);
         displayForBlueTeam(WarningB);
 
-    }
-
-    public static void buttonEffect(View button){
-        button.setOnTouchListener(new View.OnTouchListener() {
-
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: {
-                        v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
-                        v.invalidate();
-                        break;
-                    }
-                    case MotionEvent.ACTION_UP: {
-                        v.getBackground().clearColorFilter();
-                        v.invalidate();
-                        break;
-                    }
-                }
-                return false;
-            }
-        });
     }
 
 
