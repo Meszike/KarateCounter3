f9cce98be77675c6f2d33ee30dd4d477a43a91cc
Meszike <poborik@gmail.com>
2017-12-18 12:03:05 +0100

    New version

--- app/src/main/java/com/example/android/karatecounter/MainActivity.java
+++ app/src/main/java/com/example/android/karatecounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.karatecounter;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
